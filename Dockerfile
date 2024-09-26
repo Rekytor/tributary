7fa5d42cb4e7b59773981836a3e763bf900859d6
Anıl Önal <55618245+Rekytor@users.noreply.github.com>
2024-09-26 16:08:52 +0300

    Task 3
    Built a container using docker compose that has "tributary" and "redis" images.
    
    Allowed code to get "engine temperature" data from external source.

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.11
 COPY ./requirements .
 RUN pip install -r requirements
 COPY ./entrypoint.py .
-CMD exec gunicorn entrypoint:app
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "entrypoint:app"]
--- entrypoint.py
+++ entrypoint.py
@@ -1,21 +1,31 @@
-# import the flask web framework
-from flask import Flask
+import json
+import redis as redis
+from flask import Flask, request
+from loguru import logger
 
-# create a Flask server, and allow us to interact with it using the app variable
 app = Flask(__name__)
+HISTORY_LENGTH = 10
+DATA_KEY = "engine_temperature"
 
-
-# define an endpoint which accepts POST requests, and is reachable from the /record endpoint
 @app.route('/record', methods=['POST'])
 def record_engine_temperature():
-    # every time the /record endpoint is called, the code in this block is executed
-    pass
+    payload = request.get_json(force=True)
+    logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
+    engine_temperature = payload.get("engine_temperature")
+    logger.info(f"engine temperature to record is: {engine_temperature}")
 
-    # return a json payload, and a 200 status code to the client
-    return {"success": True}, 200
+    # Establishes connection to a redis server
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
+    # Pushes the latest engine temperature value
+    database.lpush(DATA_KEY, engine_temperature)
+    logger.info(f"stashed engine temperature in redis: {engine_temperature}")
 
-# practically identical to the above
-@app.route('/collect', methods=['POST'])
-def collect_engine_temperature():
+    # Ensures that the list does not exceed the length of "HISTORY_LENGTH"
+    while database.llen(DATA_KEY) > HISTORY_LENGTH:
+        database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
+
+    logger.info(f"record request successful")
     return {"success": True}, 200
